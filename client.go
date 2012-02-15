63b08499ee55acb9b9110eadd0ba8b8eb66896e6
Felix Sun <sunfmin@gmail.com>
2012-02-16 00:59:25 +0800

    be able to call top api

--- client.go
+++ client.go
@@ -2,26 +2,36 @@ package top
 
 import (
 	"encoding/json"
+	"time"
+	"sort"
+	"crypto/md5"
+	"io"
+	"net/url"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 )
 
 type Client struct {
 	AppKey string
 	SecretKey string
+	PartnerId string
 
 	signMethod string
 	format  string
-	partnerId string
 	v string
 }
 
 type Request struct {
 	Name string
 	Params map[string]string
+
+	client *Client
 }
 
 func NewClient() *Client {
 	c := &Client{}
-	c.partnerId = "top-sdk-go-20120214"
+	c.PartnerId = "top-sdk-go-20120214"
 	c.format = "json"
 	c.signMethod = "md5"
 	c.v = "2.0"
@@ -29,15 +39,69 @@ func NewClient() *Client {
 }
 
 func (client *Client) NewRequest(name string) *Request {
-	return &Request{Name: name, Params: map[string]string{}}
+	return &Request{Name: name, Params: map[string]string{}, client: client}
 }
 
-func (req *Request) Execute(result interface{}) {
-	json.Unmarshal([]byte(`{"Name": "Wednesday", "Age": 6, "Parents": ["Gomez", "Morticia"]}`), result)
+func (req *Request) Execute(result interface{}) error {
+	_, query := req.SignatureAndQueryString()
+	resp, error := http.Get("http://gw.api.taobao.com/router/rest?" + query)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	json.Unmarshal(body, result)
+	return error
 }
 
 
 func (req *Request) Param(name string, value string) {
 	req.Params[name] = value
+}
+
+func (req *Request) SignatureAndQueryString() (sign string, qs string) {
+	ps := req.makeRequestParams()
+
+	var keys []string
+
+	for k, _ := range(ps) {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	keyvalues := "";
+
+	for _, k := range(keys) {
+		keyvalues = keyvalues + k + ps[k]
+	}
+
+    beforeMd5 := req.client.SecretKey + keyvalues + req.client.SecretKey
+
+	c := md5.New()
+	io.WriteString(c, beforeMd5)
+	sign = fmt.Sprintf("%X", c.Sum(nil))
+
+	values := &url.Values{}
+	values.Add("sign", sign)
+	for _, k := range(keys) {
+		values.Add(k, ps[k])
+	}
+	qs = values.Encode()
+	return
+}
+
+
+func (req *Request) makeRequestParams() map[string]string {
+	ps := map[string]string{}
+	ps["v"] = req.client.v
+	ps["sign_method"] = req.client.signMethod
+	ps["app_key"] = req.client.AppKey
+	ps["partner_id"] = req.client.PartnerId
+	ps["method"] = req.Name
+	ps["format"] = req.client.format
+	ps["timestamp"] = time.Now().Format("2006-01-02 15:04:05")
+
+	for k, v := range(req.Params) {
+		ps[k] = v
+	}
+	return ps
 }
 
--- client_test.go
+++ client_test.go
@@ -11,16 +11,41 @@ func TestNewClient(t *testing.T) {
 
 	req := client.NewRequest("taobao.taobaoke.items.convert")
 	req.Param("nick", "孙凤民")
-	req.Param("num_iids", "13342448437")
+	req.Param("num_iids", "14009743597")
 	req.Param("fields", "click_url,num_iid,commission,commission_rate,commission_num,commission_volume")
 
 	var result map[string]interface{}
 
 	req.Execute(&result)
 
+	t.Errorf("%+v", result)
+
 	if client == nil {
 		t.Errorf("New client can't be nil")
 	}
+
+}
+
+func TestSignature(t *testing.T) {
+
+	// http://gw.api.taobao.com/router/rest?sign=76A743E65FB43AB60B8317CB10FAAC5C&timestamp=2012-02-15+23%3A02%3A05&v=2.0&app_key=12486123&method=taobao.users.get&partner_id=top-apitools&format=json&nicks=%E5%AD%99%E5%87%A4%E6%B0%91&fields=user_id,nick,sex,buyer_credit,seller_credit,location,created,last_visit,alipay_account
+
+	client := NewClient()
+	client.AppKey = "12486123"
+	client.SecretKey = "280a8edc3a899b8a1e4cb965732d2441"
+	client.PartnerId = "top-apitools"
+
+	req := client.NewRequest("taobao.users.get")
+	req.Param("nicks", "孙凤民")
+	req.Param("fields", "user_id,nick,sex,buyer_credit,seller_credit,location,created,last_visit,alipay_account")
+	req.Param("timestamp", "2012-02-15 23:02:05")
+
+	sign, _ := req.SignatureAndQueryString()
+
+	if sign != "8E170B6351CC5C6CF34DFFDC63569734" {
+		t.Errorf("Wrong signature %s", sign)
+	}
+
 
 }
 
